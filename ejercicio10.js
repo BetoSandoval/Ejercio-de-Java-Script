b164ceb9542cadf561a302d76527f2d674b93628
BetoSandoval <alberto.sa1391@gmail.com>
2021-08-24 15:02:59 -0500

    ejercicio 10

--- ejercicio10.js
+++ ejercicio10.js
@@ -2,9 +2,23 @@
 
 Dados dos  arrays, devolver array con los elementos comunes entre ambos.
 
-1.- Funcion que recibe los arrays
-2.- Ciclo for itera el primer array y lo guarda en una variable i
-3.- Ciclo for itera el segundo aray y lo guarda en una variable j
+Ejemplo:
+elementos([4, 5, 6, 7], [7, 8, 9, 7, 5]) // devualeve: [5, 7];
+
+1.- Funcion que recibe dos arrays
+2.- filtar los elementos del arraUno con .filter() .
+3.- Dentro de .filter() condicionar mediante .includes() que imprima solo aquellos items dentro de de array dos.
 
 */
 
+
+const elementos = ( arrayUno, arrayDos ) => {
+    let resultado = [];
+
+    resultado = arrayUno.filter( item => arrayDos.includes(item) );
+
+    return resultado;
+}
+
+
+console.log( elementos( [4, 5, 6, 7], [7, 4, 9, 7, 5] ) );
